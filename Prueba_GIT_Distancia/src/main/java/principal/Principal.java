14253b8dd408cd84182751b794c8e7696f787666
alber <alber@TABLET-392AMB8V>
2023-11-28 17:28:38 +0100

    Modificada clase principal

--- Prueba_GIT_Distancia/src/main/java/principal/Principal.java
+++ Prueba_GIT_Distancia/src/main/java/principal/Principal.java
@@ -4,7 +4,9 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Comienzo de programa dificil");
+		System.out.println("Sigo de programa dificil");
+		System.out.println("Añado dificultades de programa dificil");
 	}
 
 }
